384073f8deb470c1b3fbf3e4a8cb3b9091389882
Roberto Torres <59813315+rocatoro18@users.noreply.github.com>
2023-08-28 11:32:02 -0700

    Initial Commit

---
+++ test-ios-app/test_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_ios_appApp.swift
+//  test-ios-app
+//
+//  Created by Roberto Torres on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct test_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test-ios-appUITests/test_ios_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  test_ios_appUITestsLaunchTests.swift
+//  test-ios-appUITests
+//
+//  Created by Roberto Torres on 28/08/23.
+//
+
+import XCTest
+
+final class test_ios_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
